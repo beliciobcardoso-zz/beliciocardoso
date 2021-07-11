01c3391cc28ce8d44e79e75077c98e63d9018ecb
Belicio Batista Cardoso <belloinfo@gmail.com>
2021-07-11 19:34:54 -0300

    config with auth GitHub

---
+++ pages/dashboard.js
@@ -0,0 +1,18 @@
+import userAuth from "../hooks/userAuth";
+
+function Dashboard() {
+    const { user } = userAuth();
+    return (
+        <div>
+            <h1>Dashboard</h1>
+            <div>
+                <img src= {user?.photoURL} width="50" />
+                <h2>{user?.displayName}</h2>
+            </div>
+            <p>Texto teste</p>
+
+        </div>
+    )
+}
+
+export default Dashboard;
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,15 @@
+import userAuth from "../hooks/userAuth";
+
 export default function Home() {
+
+  const { user, signInGitHub } = userAuth();
+  console.log('User', user);
   return (
     <div>
-     <h1>
-       Teste da pagina em uma nova branch.
-     </h1>
+      <h1>
+        Teste da pagina em uma nova branch.
+      </h1>
+      <button onClick={() => signInGitHub()}>Entrar com GitHub</button>
     </div>
   )
 }
